0f4e4a58d17aec63dad772888771927ac28d5d1f
Julián Peñaranda <julianpeacevedo@gmail.com>
2021-08-27 20:03:31 +0200

    Change featured title in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,52 +18,24 @@ const Home = () => {
 
       <main className={styles.main}>
         <section className={styles.intro}>
-          <div className={styles.left_content}>
-            <h1 className={styles.content__title}>
-              Torneos de eSports en un único lugar. ¿Listo para entrar?{' '}
-              <span>¡Es gratis!</span>
-            </h1>
-
-            <span className={styles.explanation}>
-              Juega y organiza competiciones en todos los juegos.
+          <h1 className={styles.content_title}>
+            <span className={styles.title}>
+              Todos tus <span className={styles.highlight}>torneos</span>
             </span>
+            <span className={styles.subtitle}>en un mismo sitio</span>
+          </h1>
 
-            <div className={styles.action_button}>
-              <Link href="/">
-                <a
-                  className={`${styles.link} ${styles.button} ${styles.active}`}
-                >
-                  Entra ahora
-                </a>
-              </Link>
-              <Link href="/">
-                <a
-                  className={`${styles.link} ${styles.button} ${styles.light}`}
-                >
-                  Ver precios
-                </a>
-              </Link>
-            </div>
-          </div>
-
-          <div className={styles.right_content}>
-            <div className={styles.imageContainer}>
-              <div className={styles.wrapper_1}>
-                <img src="images/fifa.svg" className={styles.fifa} />
-              </div>
-            </div>
-
-            <div className={styles.imageContainer}>
-              <div className={styles.wrapper_2}>
-                <img src="images/overwatch.svg" className={styles.overwatch} />
-              </div>
-            </div>
-
-            <div className={styles.imageContainer}>
-              <div className={styles.wrapper_3}>
-                <img src="images/lol.svg" className={styles.lol} />
-              </div>
-            </div>
+          <div className={styles.action_button}>
+            <Link href="/">
+              <a className={`${styles.link} ${styles.button} ${styles.active}`}>
+                Entra ahora
+              </a>
+            </Link>
+            <Link href="/">
+              <a className={`${styles.link} ${styles.button} ${styles.light}`}>
+                Ver precios
+              </a>
+            </Link>
           </div>
         </section>
 

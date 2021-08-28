d533806dc801835811be4c3dd3dbdf932d4ad37e
Julián Peñaranda <julianpeacevedo@gmail.com>
2021-08-28 14:13:20 +0200

    Add button component

---
+++ components/Button.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from 'styles/components/Button.module.sass'
+
+type props = {
+  text: string
+  primary: string
+  secundary: string
+  third: string
+  light: string
+  handleOnClick: () => void
+}
+
+const Button = ({
+  text,
+  primary,
+  secundary,
+  third,
+  light,
+  handleOnClick,
+}: props): React.ReactElement => {
+  const button = classNames({
+    [styles.button]: true,
+    [styles.primary]: primary,
+    [styles.secundary]: secundary,
+    [styles.third]: third,
+    [styles.light]: light,
+  })
+
+  return (
+    <button className={button} onClick={handleOnClick}>
+      {text}
+    </button>
+  )
+}
+
+export default Button
--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
-export { default as InfluencerCard } from './InfluencerCard'
+export { default as Button } from './Button'
 export { default as Footer } from './Layout/Footer'
 export { default as Header } from './Layout/Header'
-export { default as Layout } from './Layout/Layout'
+export { default as InfluencerCard } from './InfluencerCard'
 export { default as InfluencerSlider } from './InfluencerSlider'
+export { default as Layout } from './Layout/Layout'

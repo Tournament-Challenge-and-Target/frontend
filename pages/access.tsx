f21564137aa505ca4ab3a141c46d38d4f8f4e22b
Julián Peñaranda <julianpeacevedo@gmail.com>
2021-09-07 21:06:57 +0200

    WIP acces page

--- pages/access.tsx
+++ pages/access.tsx
@@ -1,18 +1,55 @@
+import React from 'react'
+import Head from 'next/head'
+import { Button } from 'components'
 import styles from 'styles/pages/Access.module.sass'
 
-const Access = () => {
+const Access = (): React.ReactNode => {
   const handeOnSubmit = (e): void => {
     e.preventDefault()
     console.log('submit')
   }
 
   return (
-    <div className={styles.access}>
-      <div className={styles.twin_container}>
-        <form onSubmit={handeOnSubmit}>this is a form</form>
+    <>
+      <Head>
+        <title>Login / Registro - 1upgaming.gg</title>
+        <meta
+          name="description"
+          content="Página de acceso y registro para usuarios de 1UpGaming"
+        />
+      </Head>
+      <div className={styles.access}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>¡Bienvenido de nuevo!</h1>
+
+          <form className={styles.loginForm} onSubmit={handeOnSubmit}>
+            <label htmlFor="email" className={styles.label}>
+              <input
+                className={`${styles.input} ${styles.light}`}
+                type="email"
+                name="email"
+                placeholder=" "
+              />
+              <span className={styles.textLabel}>E-mail</span>
+              <div className={styles.hasBorder} />
+            </label>
+
+            <label htmlFor="password" className={styles.label}>
+              <input
+                className={`${styles.input} ${styles.light}`}
+                type="password"
+                name="password"
+                placeholder=" "
+              />
+              <span className={styles.textLabel}>Contraseña</span>
+              <div className={styles.hasBorder} />
+            </label>
+
+            <Button primary text="Identificarme" />
+          </form>
+        </div>
       </div>
-      <div className={styles.twin_container}>imagen</div>
-    </div>
+    </>
   )
 }
 

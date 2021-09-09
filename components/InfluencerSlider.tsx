62f892cd2d02125adddd7d45e660caac7584dfc1
Julián Peñaranda <julianpeacevedo@gmail.com>
2021-09-09 22:18:17 +0200

    Change images for optimized versions

--- components/InfluencerSlider.tsx
+++ components/InfluencerSlider.tsx
@@ -8,25 +8,21 @@ const InfluencerSlider = (): React.ReactElement => {
     <div className={styles.wrapper}>
       <h3 className={styles.title}>Nuestros streamers más activos</h3>
       <InfluencerCard
-        urlImg="https://images.unsplash.com/photo-1558788353-f76d92427f16?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=678&q=80"
+        urlImg="images/julian.png"
         name="Julius The Dog"
         followers={827}
       />
 
       <InfluencerCard
-        urlImg="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+        urlImg="images/noelius.png"
         name="Borja"
         followers={827}
       />
 
-      <InfluencerCard
-        urlImg="https://images.unsplash.com/photo-1504257432389-52343af06ae3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-        name="Murga"
-        followers={827}
-      />
+      <InfluencerCard urlImg="images/jorge.png" name="Murga" followers={827} />
 
       <InfluencerCard
-        urlImg="https://images.unsplash.com/photo-1506634572416-48cdfe530110?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=632&q=80"
+        urlImg="images/borja.png"
         name="NoeNoelius"
         followers={827}
       />

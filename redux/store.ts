6674b9513bf127aa93ae0ce055f4d44d30e9af59
Julián Peñaranda <julianpeacevedo@gmail.com>
2021-09-09 22:46:09 +0200

    Fix reduxdevtools typing error

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,7 @@
-import { compose, createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
+import { createStore } from 'redux'
 import rootReducers from './reducers/reducers'
 
-const enhancers = compose(
-  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__
-    ? window.__REDUX_DEVTOOLS_EXTENSION__()
-    : (f) => f
-)
-
-const store = createStore(rootReducers, enhancers)
+const store = createStore(rootReducers, devToolsEnhancer({}))
 
 export default store

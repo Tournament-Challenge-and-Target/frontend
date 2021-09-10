c009b340e0d69d815dffdcbf8dc7e52ef0a9816c
Julián Peñaranda <julianpeacevedo@gmail.com>
2021-09-10 13:48:24 +0200

    Add session storage persistor in redux

--- redux/store.ts
+++ redux/store.ts
@@ -1,7 +1,14 @@
 import { devToolsEnhancer } from 'redux-devtools-extension'
 import { createStore } from 'redux'
+import { loadState, saveState } from './sessionStorage'
 import rootReducers from './reducers/reducers'
 
-const store = createStore(rootReducers, devToolsEnhancer({}))
+const persistedState = loadState()
+
+const store = createStore(rootReducers, persistedState, devToolsEnhancer({}))
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 export default store
